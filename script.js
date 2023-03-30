8a66d470eef9076a3de3a85106f716598952603f
maheshwarihemant <122624572+maheshwarihemant@users.noreply.github.com>
2023-03-30 16:18:27 +0530

    update

--- script.js
+++ script.js
@@ -54,12 +54,19 @@ for (var i = 0; i < 3; i++) {
 // length(10);
 
 //Q.5 
-
-
-
-
-
-
+function counter(){
+        var counter = 0;
+    function IncreaseCounter() {
+        return counter += 1;
+    };
+    
+    return IncreaseCounter;
+    }
+    var counter = counter();
+    console.log(counter());
+    console.log(counter());
+    console.log(counter());
+    console.log(counter());"
 
 
 
